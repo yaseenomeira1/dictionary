0896d010e1da1135203d5da6e713ebd407e2a069
yaseenomeira1 <yaseenomeira12@gmail.com>
2023-11-15 13:08:05 -0500

    udpate

---
+++ dictionary/script.js
@@ -0,0 +1,34 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", ()=>{
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+        result.innerHTML = 
+        `<div class="word">
+        <h3>${inpWord}</h3>
+        <button onclick="playSound()">
+            <i class="fa-solid fa-volume-high"></i>
+        </button>
+    </div>
+    <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>${data[0].phonetics[0].text}</p>
+    </div>
+    <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <div class="word-example">
+       ${data[0].meanings[1].definitions[0].example} 
+    </div>`;
+    sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+    }); 
+});
+function playSound(){
+    sound.play();
+}
+ 
